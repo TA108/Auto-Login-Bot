4b9dc915756c080ec0346455ffbb2ff17c654703
tanmay <aryatanmay@gmail.com>
2025-04-06 21:01:05 +0530

    login_flow_update

--- loginBot.py
+++ loginBot.py
@@ -6,41 +6,43 @@ from selenium.webdriver.common.by import By
 import time
 
 def startBot(username, password, url):
-    # Set the path to your manually downloaded ChromeDriver
-    chrome_driver_path = r"D:\Chrome Driver\chromedriver-win64\chromedriver.exe"
+    chrome_driver_path = r".\chromedriver-win64\chromedriver.exe"
+    chrome_binary_path = r".\chrome-win64\chrome.exe"
     
-    # Set the path to your Chrome browser executable 
-    chrome_binary_path = r"D:\Chrome Driver\chrome-win64\chrome.exe"
-    os.environ["webdriver.chrome.driver"] = chrome_driver_path
-    
-    # Setup Chrome options
     chrome_options = Options()
     chrome_options.binary_location = chrome_binary_path
-    
-    # Create a Service object with the path to ChromeDriver
+    chrome_options.add_argument("--headless") 
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument(
+        "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+    )
+    chrome_options.add_argument("--disable-dev-shm-usage")
+        
     service = Service(chrome_driver_path)
-    
-    # Initialize the WebDriver with the Service object and Chrome options
     driver = webdriver.Chrome(service=service, options=chrome_options)
     
     # Open the URL in Chrome
     driver.get(url)
     
     # Find the username and password field and enter them
-    driver.find_element(By.NAME, "username_field_name").send_keys(username)
-    driver.find_element(By.NAME, "password_field_name").send_keys(password)  
+    driver.find_element(By.NAME, "username").send_keys(username)
+    driver.find_element(By.NAME, "password").send_keys(password)  
     
     # Find the login button and click it
-    driver.find_element(By.CSS_SELECTOR, "login_button_css_selector").click() 
+    driver.find_element(By.ID, "submit").click() 
     time.sleep(5)
 
     #Check if login was successful
     try:
-        dashboard_element = driver.find_element(By.ID, "dashboard_element_id")
-        print("Login successful!")
-    except:
-        print("Login failed.")
-        
+        dashboard_element = driver.find_element(By.CLASS_NAME, "post-title")
+        if "Logged In Successfully" in dashboard_element.text:
+                print("Login successful!!!")
+        else:
+            print("Login failed: Unexpected message.")
+    except  Exception as e:
+        print(f"Login failed: {e}")
+    
     driver.quit()
 
 username = "student"
